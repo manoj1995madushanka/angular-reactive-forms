ca20ba6fb52149765414408422d0516f2fcff3ac
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-23 23:54:28 +0530

    arrays of form control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -14,14 +14,24 @@ export class AppComponent implements OnInit {
     // null is default value
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-          'username': new FormControl(null, Validators.required),
-          'email': new FormControl(null, [Validators.required, Validators.email]),
-        }),
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
       'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
     });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required); // default value null and this is required
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  get controls() {
+    return (this.signupForm.get('hobbies') as FormArray).controls;
   }
 }
